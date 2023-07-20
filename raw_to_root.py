1bd42966b299bc50d6850b9984a760da0b4b11e7
Matias Senger <m.senger@hotmail.com>
2023-07-20 11:08:11 +0200

    Added logging debug messages and introduced a fix for something I don't understand why it fails...

--- raw_to_root.py
+++ raw_to_root.py
@@ -1,6 +1,7 @@
 from the_bureaucrat.bureaucrats import RunBureaucrat # https://github.com/SengerM/the_bureaucrat
 from pathlib import Path
 import subprocess
+import logging
 
 PATH_TO_caenCliRootWF = Path.home()/'code/Jordis_docker/eudaq/bin/caenCliRootWF'
 
@@ -22,7 +23,11 @@ def raw_to_root(bureaucrat:RunBureaucrat, force:bool=False):
 		paht_to_directory_in_which_to_save_the_root_files = employee.path_to_directory_of_my_task/'root_files'
 		paht_to_directory_in_which_to_save_the_root_files.mkdir()
 		for path_to_raw_file in (employee.path_to_run_directory/'raw').iterdir():
-			subprocess.run([str(PATH_TO_caenCliRootWF), '-i', str(path_to_raw_file), '-o', str(paht_to_directory_in_which_to_save_the_root_files/path_to_raw_file.name.replace('.raw','.root'))])
+			logging.debug(f'About to process {path_to_raw_file}')
+			subprocess.run(
+				[str(PATH_TO_caenCliRootWF), '-i', str(path_to_raw_file), '-o', str(paht_to_directory_in_which_to_save_the_root_files/path_to_raw_file.name.replace('.raw','.root'))],
+				cwd = PATH_TO_caenCliRootWF.parent,
+			)
 	
 if __name__=='__main__':
 	import argparse
