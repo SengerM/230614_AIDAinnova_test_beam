daf13679c394e69e3740081c3cfd7291561d7a02
Matias Senger <m.senger@hotmail.com>
2023-07-24 16:22:22 +0200

    Added HTML file with all the waveforms together

--- analyze_event.py
+++ analyze_event.py
@@ -8,6 +8,7 @@ import plotly.graph_objects as go
 import logging
 import utils
 from parse_waveforms import plot_waveform
+import dominate # https://github.com/Knio/dominate
 
 def analyze_event(bureaucrat:RunBureaucrat, n_run:int, n_event:int):
 	bureaucrat.check_these_tasks_were_run_successfully(['raw_to_root','batch_info'])
@@ -51,8 +52,8 @@ def analyze_event(bureaucrat:RunBureaucrat, n_run:int, n_event:int):
 	signals_connections.sort_index(inplace=True)
 	
 	with bureaucrat.handle_task('analyze_specific_events', drop_old_data=False) as employee:
-		save_plots_here = employee.path_to_directory_of_my_task/f'n_run_{n_run}_n_event_{n_event}'
-		save_plots_here.mkdir()
+		save_plots_here = employee.path_to_directory_of_my_task/f'n_run_{n_run}_n_event_{n_event}/waveforms'
+		save_plots_here.mkdir(parents=True, exist_ok=False)
 		for n_CAEN,idx in enumerate(indices_where_to_look_for):
 			CAEN_name = waveforms['producer'].array(entry_start=idx,entry_stop=idx+1, library='np')[0]
 			CAEN_name = CAEN_name.replace('CAEN_','') # In some point in EUDAQ or in the raw to root conversion, the CAENs names are prepended "CAEN_", which is annoying...
@@ -72,12 +73,23 @@ def analyze_event(bureaucrat:RunBureaucrat, n_run:int, n_event:int):
 				)
 				fig = plot_waveform(signal, peak_start_time=False)
 				fig.update_layout(
-					title = f'Event {n_event}, DUT: {DUT_name} ({row},{col}), CAEN: {CAEN_name} {CAEN_channel_name}<br><sup>{bureaucrat.run_name}</sup>',
+					title = f'Run {n_run}, event {n_event}, DUT: {DUT_name} ({row},{col}), CAEN: {CAEN_name} {CAEN_channel_name}<br><sup>{bureaucrat.run_name}</sup>',
 				)
 				fig.write_html(
 					save_plots_here/f'{DUT_name}_{row}{col}_CAEN_{CAEN_name}.html',
 					include_plotlyjs = 'cdn',
 				)
+			
+			document_title = f'Waveforms for run {n_run}, event {n_event}'
+			html_doc = dominate.document(title=document_title)
+			with html_doc:
+				dominate.tags.h1(document_title)
+				with dominate.tags.div(style='display: flex; flex-direction: column; width: 100%;'):
+					for path_to_plot in sorted(save_plots_here.iterdir()):
+						path_to_plot = path_to_plot.relative_to(path_to_plot.parent.parent)
+						dominate.tags.iframe(src=str(path_to_plot), style=f'height: 88vh; min-height: 333px; width: 100%; min-width: 888px; border-style: solid;')
+			with open(save_plots_here.parent/f'waveforms.html', 'w') as ofile:
+				print(html_doc, file=ofile)
 
 if __name__=='__main__':
 	import argparse
