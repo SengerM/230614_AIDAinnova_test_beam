d39aafbb0c9ee29c9400143dec66a126ac92f5cf
Matias Senger <m.senger@hotmail.com>
2023-07-24 13:15:02 +0200

    New function to create something that looks like a beta scan for quick time resolution analysis

---
+++ filter_events.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+import numpy
+import pandas
+from the_bureaucrat.bureaucrats import RunBureaucrat # https://github.com/SengerM/the_bureaucrat
+import logging
+import utils
+import sqlite3
+from huge_dataframe.SQLiteDataFrame import SQLiteDataFrameDumper
+
+def filter_two_pixels_for_beta_scan_like_time_resolution(bureaucrat:RunBureaucrat, pixels:dict, n_event_batch_size:int=11111):
+	"""Creates a new sub run that looks like a beta scan.
+	
+	Arguments
+	---------
+	bureaucrat: RunBureaucrat
+		A bureaucrat pointing to the run in which to process this.
+	pixels: dict
+		A dictionary of the form
+		```
+		{'TI143':(0,0), 'time_reference_1':(1,0)}
+		```
+		i.e. with two DUTs names and one pixel per DUT.
+	"""
+	bureaucrat.check_these_tasks_were_run_successfully('parse_waveforms')
+	
+	if len(pixels) != 2 or any([len(_) != 2 for DUT_name,_ in pixels.items()]):
+		raise ValueError(f'`pixels` is wrong, please check.')
+	
+	signals_connections = utils.load_setup_configuration_info(bureaucrat)
+	we_are_interested_in = []
+	for DUT_name,_ in pixels.items():
+		row,col = _
+		we_are_interested_in.append(signals_connections.query(f'DUT_name=={repr(DUT_name)} and row=={row} and col=={col}'))
+	we_are_interested_in = pandas.concat(we_are_interested_in)
+	we_are_interested_in.set_index(['DUT_name','row','col'], inplace=True)
+	
+	with bureaucrat.handle_task('beta_scan_like_subsets', drop_old_data=False) as employee:
+		subrun = employee.create_subrun('_'.join([f'{DUT_name}_{pixels[DUT_name][0]}{pixels[DUT_name][1]}' for DUT_name in sorted(pixels)]))
+		with subrun.handle_task('beta_scan') as subemployee:
+			with SQLiteDataFrameDumper(subemployee.path_to_directory_of_my_task/'parsed_from_waveforms.sqlite', dump_after_n_appends=999) as data_dumper:
+				current_lowest_n_waveform = 0
+				for sqlite_file_path in (bureaucrat.path_to_directory_of_task('parse_waveforms')/'parsed_data').iterdir():
+					n_run = int(sqlite_file_path.name.split('_')[0].replace('run',''))
+					connection = sqlite3.connect(sqlite_file_path)
+					n_event_low = 0
+					while True:
+						n_event_high = n_event_low+n_event_batch_size
+						data = pandas.read_sql(f'SELECT * FROM dataframe_table WHERE n_event>={n_event_low} AND n_event<{n_event_high}', connection)
+						data['n_run'] = n_run
+						data.set_index(['n_run','n_event','n_CAEN','CAEN_n_channel'], inplace=True)
+						if len(data)==0:
+							break
+						
+						keep_this = []
+						for idx,info in we_are_interested_in.iterrows():
+							DUT_name,row,col = idx
+							keep_this.append(
+								data.query(f'n_CAEN=={info["n_CAEN"]} and CAEN_n_channel=={info["CAEN_n_channel"]}')
+							)
+						keep_this = pandas.concat(keep_this)
+						keep_this = keep_this.reset_index(drop=False).merge(we_are_interested_in.reset_index(drop=False).set_index(['n_CAEN','CAEN_n_channel'])[['DUT_name','row','col']], on=['n_CAEN','CAEN_n_channel'])
+						keep_this['n_trigger'] = keep_this['n_event']
+						keep_this['signal_name'] = keep_this[['DUT_name','row','col']].apply(lambda x: f'{x["DUT_name"]}_{x["row"]}{x["col"]}', axis=1)
+						keep_this.set_index(['n_trigger','signal_name'], inplace=True)
+						keep_this['n_waveform'] = numpy.array([n_waveform for n_waveform in range(current_lowest_n_waveform,current_lowest_n_waveform+len(keep_this))])
+						for col in ['Amplitude (V)','Collected charge (V s)']:
+							keep_this[col] *= -1
+						data_dumper.append(keep_this)
+						n_event_low = n_event_high
+						current_lowest_n_waveform += len(keep_this)
+						logging.info(f'{n_event_low} events were processed.')
+
+if __name__=='__main__':
+	import argparse
+	from grafica.plotly_utils.utils import set_my_template_as_default
+	
+	set_my_template_as_default()
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--dir',
+		metavar = 'path', 
+		help = 'Path to the base measurement directory.',
+		required = True,
+		dest = 'directory',
+		type = str,
+	)
+	
+	args = parser.parse_args()
+	bureaucrat = RunBureaucrat(Path(args.directory))
+	filter_two_pixels_for_beta_scan_like_time_resolution(
+		bureaucrat, 
+		pixels = {'TI143':(0,0), 'time_reference_1':(0,0)},
+	)
