532d501750cda92ac194a89197e43d9ff4ce26be
Matias Senger <m.senger@hotmail.com>
2023-07-24 17:33:22 +0200

    Aesthetic fix because I cannot stand imperfection

--- filter_events.py
+++ filter_events.py
@@ -66,6 +66,7 @@ def filter_two_pixels_for_beta_scan_like_time_resolution(bureaucrat:RunBureaucra
 						this_batch_n_events = keep_this['n_event'].drop_duplicates().to_frame()
 						this_batch_n_events['n_trigger'] = numpy.arange(start=absolute_n_trigger, stop=absolute_n_trigger+len(this_batch_n_events))
 						absolute_n_trigger += len(this_batch_n_events)
+						n_event_low += len(this_batch_n_events)
 						this_batch_n_events.set_index('n_event', inplace=True)
 						this_batch_n_events = this_batch_n_events['n_trigger']
 						keep_this = keep_this.join(this_batch_n_events, on='n_event')
@@ -75,8 +76,7 @@ def filter_two_pixels_for_beta_scan_like_time_resolution(bureaucrat:RunBureaucra
 						for col in ['Amplitude (V)','Collected charge (V s)','Whole signal integral (V s)']:
 							keep_this[col] *= -1
 						data_dumper.append(keep_this)
-						logging.info(f'{n_event_high} events from file {sqlite_file_path.name}, total {absolute_n_trigger}')
-						n_event_low = n_event_high
+						logging.info(f'{n_event_low} events from file {sqlite_file_path.name}, total {absolute_n_trigger}')
 						current_lowest_n_waveform += len(keep_this)
 						
 
