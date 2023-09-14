a5b2bbb10c295d33d33a05064c9bdd87a499be54
Matias Senger <m.senger@hotmail.com>
2023-09-14 16:12:25 +0200

    Implemented some functionalities for the August TB data

--- utils.py
+++ utils.py
@@ -6,22 +6,68 @@ def save_dataframe(df, name:str, location:Path):
 	for extension,method in {'pickle':df.to_pickle,'csv':df.to_csv}.items():
 		method(location/f'{name}.{extension}')
 
+def which_test_beam_campaign(bureaucrat:RunBureaucrat):
+	"""Returns the name of the test beam campaign, one of '230614_June' or
+	'230830_August'."""
+	if bureaucrat.path_to_run_directory.parent.parent.name == '230614_June':
+		return '230614_June'
+	elif bureaucrat.path_to_run_directory.parent.parent.name == '230830_August':
+		return '230830_August'
+
 def setup_batch_info(bureaucrat:RunBureaucrat):
 	"""Add some batch-wise information needed for the analysis, like
 	for example a link to the setup connection spreadsheet."""
-	PATH_TO_SETUP_CONNECTIONS_FILES = Path.home()/'June_test_beam_data/AIDAInnova_June/setup_connections'
+	def setup_batch_info_June_test_beam(bureaucrat:RunBureaucrat):
+		PATH_TO_SETUP_CONNECTIONS_FILES = Path.home()/'June_test_beam_data/AIDAInnova_June/setup_connections'
+		
+		with bureaucrat.handle_task('batch_info') as employee:
+			with open(bureaucrat.path_to_run_directory/'n_batch', 'r') as ifile:
+				n_batch = int(ifile.readline())
+			path_to_setup_connection_ods = PATH_TO_SETUP_CONNECTIONS_FILES/f'Batch{n_batch}.ods'
+			
+			have_to_navigate_backwards = [_ for i,_ in enumerate(employee.path_to_directory_of_my_task.parts) if _!=path_to_setup_connection_ods.parts[i]]
+			have_to_navigate_upwards_afterward = [_ for i,_ in enumerate(path_to_setup_connection_ods.parts) if _!=employee.path_to_directory_of_my_task.parts[i]]
+			
+			relative_path_to_setup_connection_ods = Path('/'.join(['..' for i in have_to_navigate_backwards]))/Path('/'.join(have_to_navigate_upwards_afterward))
+			
+			(employee.path_to_directory_of_my_task/'setup_connections.ods').symlink_to(relative_path_to_setup_connection_ods)
 	
-	with bureaucrat.handle_task('batch_info') as employee:
-		with open(bureaucrat.path_to_run_directory/'n_batch', 'r') as ifile:
-			n_batch = int(ifile.readline())
-		path_to_setup_connection_ods = PATH_TO_SETUP_CONNECTIONS_FILES/f'Batch{n_batch}.ods'
-		
-		have_to_navigate_backwards = [_ for i,_ in enumerate(employee.path_to_directory_of_my_task.parts) if _!=path_to_setup_connection_ods.parts[i]]
-		have_to_navigate_upwards_afterward = [_ for i,_ in enumerate(path_to_setup_connection_ods.parts) if _!=employee.path_to_directory_of_my_task.parts[i]]
-		
-		relative_path_to_setup_connection_ods = Path('/'.join(['..' for i in have_to_navigate_backwards]))/Path('/'.join(have_to_navigate_upwards_afterward))
-		
-		(employee.path_to_directory_of_my_task/'setup_connections.ods').symlink_to(relative_path_to_setup_connection_ods)
+	def setup_batch_info_August_test_beam(bureaucrat:RunBureaucrat):
+		with bureaucrat.handle_task('batch_info') as employee:
+			with open(bureaucrat.path_to_run_directory/'n_batch', 'r') as ifile:
+				n_batch = int(ifile.readline())
+			planes_definition = pandas.read_csv(
+				'https://docs.google.com/spreadsheets/d/e/2PACX-1vTuRXCnGCPu8nuTrrh_6M_QaBYwVQZfmLX7cr6OlM-ucf9yx3KIbBN4XBQxc0fTp-O26Y2QIOCkgP98/pub?gid=0&single=true&output=csv',
+				dtype = dict(
+					batch_number = int,
+					plane_number = int,
+					DUT_name = str,
+					orientation = str,
+					high_voltage_source = str,
+					low_voltage_source = str,
+				),
+				index_col = ['batch_number','plane_number'],
+			)
+			pixels_definition = pandas.read_csv(
+				'https://docs.google.com/spreadsheets/d/e/2PACX-1vTuRXCnGCPu8nuTrrh_6M_QaBYwVQZfmLX7cr6OlM-ucf9yx3KIbBN4XBQxc0fTp-O26Y2QIOCkgP98/pub?gid=1673457618&single=true&output=csv',
+				dtype = dict(
+					batch_number = int,
+					plane_number = int,
+					chubut_channel_number = int,
+					digitizer_name = str,
+					digitizer_channel_name = str,
+					row = int,
+					col = int,
+				),
+				index_col = ['batch_number','plane_number'],
+			)
+			for name,df in {'planes_definition':planes_definition, 'pixels_definition':pixels_definition}.items():
+				save_dataframe(df.query(f'batch_number=={n_batch}'), name, employee.path_to_directory_of_my_task)
+	
+	if which_test_beam_campaign(bureaucrat) == '230614_June':
+		setup_batch_info_June_test_beam(bureaucrat)
+	elif which_test_beam_campaign(bureaucrat) == '230830_August':
+		setup_batch_info_August_test_beam(bureaucrat)
 
 CAENs_CHANNELS_MAPPING_TO_INTEGERS = pandas.DataFrame(
 	# This codification into integers comes from the producer, see in line 208 of `CAENDT5742Producer.py`. The reason is that EUDAQ can only handle integers tags, or something like this.
