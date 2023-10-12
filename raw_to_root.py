88bf1a90116c7f7b55fe99a302ac939165ad1f5c
Matias Senger <m.senger@hotmail.com>
2023-10-12 12:09:03 +0200

    Implemented `raw_to_root_whole_TB_campaign`

--- raw_to_root.py
+++ raw_to_root.py
@@ -22,10 +22,13 @@ def raw_to_root(bureaucrat:RunBureaucrat, force:bool=False):
 		return
 	
 	with bureaucrat.handle_task('raw_to_root') as employee:
+		logging.info(f'Processing {bureaucrat.path_to_run_directory}...')
 		path_to_raw_file = bureaucrat.path_to_directory_of_task('raw')/f'{bureaucrat.run_name}.raw'
 		result = subprocess.run(
 			[str(PATH_TO_caenCliRootWF), '-i', str(path_to_raw_file), '-o', str(employee.path_to_directory_of_my_task/path_to_raw_file.name.replace('.raw','.root'))],
 			cwd = PATH_TO_caenCliRootWF.parent,
+			stdout = subprocess.DEVNULL,
+			stderr = subprocess.STDOUT,
 		)
 		try:
 			result.check_returncode()
@@ -42,9 +45,23 @@ def raw_to_root_whole_batch(batch_bureaucrat:RunBureaucrat, force:bool=False):
 	
 	for TB_run_bureaucrat in batch_bureaucrat.list_subruns_of_task('runs'):
 		raw_to_root(TB_run_bureaucrat, force)
+
+def raw_to_root_whole_TB_campaign(TB_campaign_bureaucrat:RunBureaucrat, force:bool=False):
+	TB_campaign_bureaucrat.check_these_tasks_were_run_successfully('batches')
 	
+	for batch in TB_campaign_bureaucrat.list_subruns_of_task('batches'):
+		raw_to_root_whole_batch(batch, force)
+
 if __name__=='__main__':
 	import argparse
+	import sys
+	
+	logging.basicConfig(
+		stream = sys.stderr, 
+		level = logging.INFO,
+		format = '%(asctime)s|%(levelname)s|%(message)s',
+		datefmt = '%H:%M:%S',
+	)
 	
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--dir',
@@ -65,4 +82,11 @@ if __name__=='__main__':
 	args = parser.parse_args()
 	bureaucrat = RunBureaucrat(Path(args.directory))
 	
-	raw_to_root_whole_batch(bureaucrat, force=args.force)
+	if bureaucrat.was_task_run_successfully('batches'):
+		raw_to_root_whole_TB_campaign(bureaucrat, force=args.force)
+	elif bureaucrat.was_task_run_successfully('runs'):
+		raw_to_root_whole_batch(bureaucrat, force=args.force)
+	elif bureaucrat.was_task_run_successfully('raw'):
+		raw_to_root(bureaucrat, force=args.force)
+	else:
+		raise RuntimeError(f'Dont know how to process run {repr(bureaucrat.run_name)} located in {bureaucrat.path_to_run_directory}')
