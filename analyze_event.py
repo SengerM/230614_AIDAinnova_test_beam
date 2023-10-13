868d8212f49e833182d7adf14a4b24c3d9a66faf
Matias Senger <m.senger@hotmail.com>
2023-10-13 14:29:54 +0200

    Updated

--- analyze_event.py
+++ analyze_event.py
@@ -10,19 +10,30 @@ import utils
 from parse_waveforms import plot_waveform
 import dominate # https://github.com/Knio/dominate
 
-def analyze_event(bureaucrat:RunBureaucrat, n_run:int, n_event:int):
-	bureaucrat.check_these_tasks_were_run_successfully(['raw_to_root','batch_info'])
+def create_event_within_run(bureaucrat:RunBureaucrat, n_event:int, if_exists:str='override')->RunBureaucrat:
+	bureaucrat.check_these_tasks_were_run_successfully('raw') # So we are sure this points to a "run node bureaucrat".
 	
-	path_to_root_file = [p for p in (bureaucrat.path_to_directory_of_task('raw_to_root')/'root_files').iterdir() if str(n_run) in p.name.split('_')[0]]
-	if len(path_to_root_file) != 1:
-		raise RuntimeError(f"Cannot locate root file for n_run {n_run} in {bureaucrat.path_to_directory_of_task('raw_to_root')/'root_files'}. ")
-	path_to_root_file = path_to_root_file[0]
+	if not isinstance(n_event, int):
+		raise TypeError(f'`n_event` must be an integer number, received an object of type {type(n_event)}')
 	
-	ifile = uproot.open(path_to_root_file)
+	with bureaucrat.handle_task('events', drop_old_data=False) as events_employee:
+		event = events_employee.create_subrun(f'event_{n_event}', if_exists=if_exists)
+		with event.handle_task('this_is_an_event'):
+			pass
+	return event
+
+def plot_event_waveforms(bureaucrat:RunBureaucrat):
+	bureaucrat.check_these_tasks_were_run_successfully('this_is_an_event')
 	
-	metadata = ifile['Metadata']
-	waveforms = ifile['Waveforms']
+	n_event = int(bureaucrat.run_name.replace('event_',''))
 	
+	bureaucrat_of_the_TB_run = bureaucrat.parent
+	bureaucrat_of_the_TB_run.check_these_tasks_were_run_successfully('raw_to_root')
+	
+	root_file = uproot.open(bureaucrat_of_the_TB_run.path_to_directory_of_task('raw_to_root')/f'{bureaucrat_of_the_TB_run.run_name}.root')
+	metadata = root_file['Metadata']
+	waveforms = root_file['Waveforms']
+
 	CAENs_channels_numbers = {}
 	for n_CAEN,list_of_lists_of_channels in enumerate(metadata['channel'].array(library='np')):
 		channels_numbers = [int(_) for listita in list_of_lists_of_channels for _ in listita]
@@ -31,32 +42,21 @@ def analyze_event(bureaucrat:RunBureaucrat, n_run:int, n_event:int):
 	sampling_frequency = metadata['sampling_frequency_MHz'].array()[0]*1e6
 	samples_per_waveform = metadata['samples_per_waveform'].array()[0]
 	time_array = numpy.linspace(0,(samples_per_waveform-1)/sampling_frequency,samples_per_waveform)
-	
+
 	n_events = waveforms['event'].array(library='np')
 	indices_where_to_look_for = numpy.where(n_events==n_event)[0]
 	if len(indices_where_to_look_for) == 0:
 		raise ValueError(f'n_event {n_event} not present in run number {n_run}. ')
-	
-	planes = pandas.read_excel(bureaucrat.path_to_directory_of_task('batch_info')/'setup_connections.ods', sheet_name='planes', index_col='plane_number')
-	signals_connections = pandas.read_excel(bureaucrat.path_to_directory_of_task('batch_info')/'setup_connections.ods', sheet_name='signals', index_col='plane_number')
-	signals_connections = signals_connections.reset_index(drop=False).merge(
-		utils.CAENs_CHANNELS_MAPPING_TO_INTEGERS.set_index('CAEN_channel_name')['CAEN_n_channel'],
-		on = 'CAEN_channel_name',
-	)
-	
-	signals_connections = signals_connections.merge(
-		planes['DUT_name'],
-		on = 'plane_number',
-	)
+
+	signals_connections = utils.load_setup_configuration_info(bureaucrat.parent.parent)
 	signals_connections.set_index(['CAEN_name','CAEN_n_channel'], inplace=True)
-	signals_connections.sort_index(inplace=True)
-	
-	with bureaucrat.handle_task('analyze_specific_events', drop_old_data=False) as employee:
-		save_plots_here = employee.path_to_directory_of_my_task/f'n_run_{n_run}_n_event_{n_event}/waveforms'
-		save_plots_here.mkdir(parents=True, exist_ok=False)
+
+	with bureaucrat.handle_task('plot_waveforms') as employee:
+		save_plots_here = employee.path_to_directory_of_my_task/'waveforms_plots'
+		save_plots_here.mkdir()
 		for n_CAEN,idx in enumerate(indices_where_to_look_for):
 			CAEN_name = waveforms['producer'].array(entry_start=idx,entry_stop=idx+1, library='np')[0]
-			CAEN_name = CAEN_name.replace('CAEN_','') # In some point in EUDAQ or in the raw to root conversion, the CAENs names are prepended "CAEN_", which is annoying...
+			CAEN_name = CAEN_name.replace('CAEN_','') # In some point in EUDAQ or in the raw to root conversion.
 			
 			for i,wf in enumerate(waveforms['voltages'].array(entry_start=idx,entry_stop=idx+1, library='np')[0]):
 				CAEN_n_channel = CAENs_channels_numbers[n_CAEN][i]
@@ -73,7 +73,7 @@ def analyze_event(bureaucrat:RunBureaucrat, n_run:int, n_event:int):
 				)
 				fig = plot_waveform(signal, peak_start_time=False)
 				fig.update_layout(
-					title = f'Run {n_run}, event {n_event}, DUT: {DUT_name} ({row},{col}), CAEN: {CAEN_name} {CAEN_channel_name}<br><sup>{bureaucrat.run_name}</sup>',
+					title = f'DUT: {DUT_name} ({row},{col}), CAEN: {CAEN_name} {CAEN_channel_name}<br><sup>{bureaucrat.pseudopath}</sup>',
 				)
 				fig.update_traces(
 					hoverinfo = 'skip',
@@ -83,7 +83,7 @@ def analyze_event(bureaucrat:RunBureaucrat, n_run:int, n_event:int):
 					include_plotlyjs = 'cdn',
 				)
 			
-			document_title = f'Waveforms for run {n_run}, event {n_event}'
+			document_title = f'Waveforms for {bureaucrat.pseudopath}'
 			html_doc = dominate.document(title=document_title)
 			with html_doc:
 				dominate.tags.h1(document_title)
@@ -93,6 +93,13 @@ def analyze_event(bureaucrat:RunBureaucrat, n_run:int, n_event:int):
 						dominate.tags.iframe(src=str(path_to_plot), style=f'height: 88vh; min-height: 333px; width: 100%; min-width: 888px; border-style: solid;')
 			with open(save_plots_here.parent/f'waveforms.html', 'w') as ofile:
 				print(html_doc, file=ofile)
+			logging.info(f'Waveforms were plotted for {bureaucrat.pseudopath} ✅')
+
+def analyze_event_of_run(bureaucrat:RunBureaucrat, n_event:int):
+	bureaucrat.check_these_tasks_were_run_successfully('raw') # So we are sure this points to a "run node bureaucrat".
+	
+	event_bureaucrat = create_event_within_run(bureaucrat, n_event, if_exists='override')
+	plot_event_waveforms(event_bureaucrat)
 
 if __name__=='__main__':
 	import argparse
@@ -111,31 +118,22 @@ if __name__=='__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--dir',
 		metavar = 'path', 
-		help = 'Path to the base measurement directory.',
+		help = 'Path to a run.',
 		required = True,
 		dest = 'directory',
 		type = str,
 	)
-	parser.add_argument('--n_run',
-		metavar = 'N',
-		help = 'Number of run in which to look for the event (i.e. the EUDAQ run number).',
-		required = True,
-		dest = 'n_run',
-		type = int,
-	)
 	parser.add_argument('--n_event',
 		metavar = 'N',
-		help = 'Number of event within the EUDAQ run.',
+		help = 'Number of event within the run.',
 		required = True,
 		dest = 'n_event',
 		type = int,
 	)
 	
-	
 	args = parser.parse_args()
 	bureaucrat = RunBureaucrat(Path(args.directory))
-	analyze_event(
-		bureaucrat, 
-		n_run = args.n_run,
+	analyze_event_of_run(
+		bureaucrat,
 		n_event = args.n_event,
 	)
