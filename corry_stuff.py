dd8e7eb9209ec49e4948262e08f59f4c995bce73
Matias Senger <m.senger@hotmail.com>
2024-03-14 13:49:41 +0100

    Now it makes a copy of the `.geo` file also for the masking process

--- corry_stuff.py
+++ corry_stuff.py
@@ -25,31 +25,39 @@ def replace_arguments_in_file_template(file_template:Path, output_file:Path, arg
 					line = line.replace(f'ASD({arg_name})DSA',arg_val)
 				print(line, file=ofile, end='')
 
-def corry_mask_noisy_pixels(bureaucrat:RunBureaucrat, corry_container_id:str, force:bool=False, silent_corry:bool=False):
+def corry_mask_noisy_pixels(EUDAQ_run:RunBureaucrat, corry_container_id:str, force:bool=False, silent_corry:bool=False):
 	"""Runs the routine to mask the noisy pixels using corryvreckan. The
-	`bureaucrat` must point to a 'raw run' run, for example to this:
+	`EUDAQ_run` must point to a 'EUDAQ run' run directory, for example to this:
 	TB_data_analysis/campaigns/subruns/230614_June/batches/subruns/batch_2_200V/runs/subruns/run000930_230623222123"""
 	TEMPLATE_FILES_DIRECTORY = Path(__file__).parent.resolve()/Path('corry_templates/01_mask_noisy_pixels')
 	
-	bureaucrat.check_these_tasks_were_run_successfully('raw')
+	EUDAQ_run.check_these_tasks_were_run_successfully('raw')
+	if not (EUDAQ_run.parent.path_to_run_directory/'corry_geometry_for_this_batch.geo').is_file():
+		raise RuntimeError(f'Cannot find `corry_geometry_for_this_batch.geo` file in parent from {EUDAQ_run.pseudopath}, i.e. cannot find it in {EUDAQ_run.parent.path_to_run_directory}')
 	
 	TASK_NAME = 'corry_mask_noisy_pixels'
 	
-	if force==False and bureaucrat.was_task_run_successfully(TASK_NAME):
-		logging.info(f'Found an already successfull execution of {TASK_NAME} within {bureaucrat.run_name}, will not do anything.')
+	if force==False and EUDAQ_run.was_task_run_successfully(TASK_NAME):
+		logging.info(f'Found an already successfull execution of {TASK_NAME} within {EUDAQ_run.pseudopath}, will not do anything.')
 		return
 	
-	with bureaucrat.handle_task(TASK_NAME) as employee:
+	with EUDAQ_run.handle_task(TASK_NAME) as employee:
 		path_to_where_to_save_the_config_files = employee.path_to_directory_of_my_task
+		
+		# Create a copy of the `.geo` file for the masking process:
+		with open(EUDAQ_run.parent.path_to_run_directory/'corry_geometry_for_this_batch.geo', 'r') as ifile:
+			with open(employee.path_to_directory_of_my_task/'corry_geometry_for_this_batch_masking.geo', 'w') as ofile:
+				for line in ifile:
+					print(line, file=ofile, end='')
 		
 		arguments_for_config_files = {
 			'01_mask_noisy_pixels.conf': dict(
 				OUTPUT_DIRECTORY = f'"corry_output"',
-				GEOMETRY_FILE = f'"../../../../corry_geometry_for_this_batch.geo"',
-				PATH_TO_RAW_FILE = f'"../raw/{bureaucrat.run_name}.raw"',
+				GEOMETRY_FILE = f'"corry_geometry_for_this_batch_masking.geo"',
+				PATH_TO_RAW_FILE = f'"../raw/{EUDAQ_run.run_name}.raw"',
 			),
 			'tell_corry_docker_to_run_this.sh': dict(
-				WORKING_DIRECTORY = str(utils.get_run_directory_within_corry_docker(bureaucrat)/employee.task_name),
+				WORKING_DIRECTORY = str(utils.get_run_directory_within_corry_docker(EUDAQ_run)/employee.task_name),
 			),
 		}
 		
@@ -63,47 +71,49 @@ def corry_mask_noisy_pixels(bureaucrat:RunBureaucrat, corry_container_id:str, fo
 		# Make the script executable for docker:
 		subprocess.run(['chmod','+x',str(employee.path_to_directory_of_my_task/'tell_corry_docker_to_run_this.sh')])
 
-		logging.info(f'Running mask noisy pixels on {bureaucrat.path_to_run_directory}...')
+		logging.info(f'Running mask noisy pixels on {EUDAQ_run.pseudopath}...')
 		result = utils.run_commands_in_docker_container(
-			command = str(utils.get_run_directory_within_corry_docker(bureaucrat)/employee.task_name/'tell_corry_docker_to_run_this.sh'),
+			command = str(utils.get_run_directory_within_corry_docker(EUDAQ_run)/employee.task_name/'tell_corry_docker_to_run_this.sh'),
 			container_id = corry_container_id,
 			stdout = subprocess.DEVNULL if silent_corry == True else None,
 			stderr = subprocess.STDOUT if silent_corry == True else None,
 		)
 		result.check_returncode()
-		logging.info(f'Mask noisy pixels was completed for {bureaucrat.pseudopath} ✅')
+		logging.info(f'Mask noisy pixels was completed for {EUDAQ_run.pseudopath} ✅')
 
-def corry_align_telescope(bureaucrat:RunBureaucrat, corry_container_id:str, force:bool=False, silent_corry:bool=False):
+def corry_align_telescope(EUDAQ_run:RunBureaucrat, corry_container_id:str, force:bool=False, silent_corry:bool=False):
 	"""Runs the routine to align the telescope using corryvreckan. The
-	`bureaucrat` must point to a 'raw run' run, for example to this:
+	`EUDAQ_run` must point to a 'raw run' run, for example to this:
 	TB_data_analysis/campaigns/subruns/230614_June/batches/subruns/batch_2_200V/runs/subruns/run000930_230623222123"""
 	TEMPLATE_FILES_DIRECTORY = Path(__file__).parent.resolve()/Path('corry_templates/02_align_telescope')
 	
 	TASK_NAME = 'corry_align_telescope'
 	
-	if force==False and bureaucrat.was_task_run_successfully(TASK_NAME):
-		logging.info(f'Found an already successfull execution of {TASK_NAME} within {bureaucrat.run_name}, will not do anything.')
+	if force==False and EUDAQ_run.was_task_run_successfully(TASK_NAME):
+		logging.info(f'Found an already successfull execution of {TASK_NAME} within {EUDAQ_run.pseudopath}, will not do anything.')
 		return
 	
-	bureaucrat.check_these_tasks_were_run_successfully(['raw','corry_mask_noisy_pixels'])
+	EUDAQ_run.check_these_tasks_were_run_successfully(['raw','corry_mask_noisy_pixels'])
 	
-	with bureaucrat.handle_task(TASK_NAME) as employee:
-		path_to_raw_file_within_docker_container = f'../raw/{bureaucrat.run_name}.raw'
+	with EUDAQ_run.handle_task(TASK_NAME) as employee:
+		path_to_raw_file_within_docker_container = f'../raw/{EUDAQ_run.run_name}.raw'
 
 		# First of all, copy the geometry file adding the mask noisy pixels extra lines.
 		path_to_geometry_file_with_noisy_pixels_mask = employee.path_to_directory_of_my_task/'corry_geometry_for_this_batch_with_noisy_pixels_mask.geo'
-		with open(bureaucrat.parent.path_to_run_directory/'corry_geometry_for_this_batch.geo', 'r') as ifile:
+		with open(EUDAQ_run.parent.path_to_run_directory/'corry_geometry_for_this_batch.geo', 'r') as ifile:
 			with open(path_to_geometry_file_with_noisy_pixels_mask, 'w') as ofile:
 				for line in ifile:
 					print(line, file=ofile, end='')
 					for n_mimosa in [0,1,2,3,4,5]:
 						if f'[MIMOSA26_{n_mimosa}]' in line:
 							print(f'mask_file = "../corry_mask_noisy_pixels/corry_output/MaskCreator/MIMOSA26_{n_mimosa}/mask_MIMOSA26_{n_mimosa}.txt"', file=ofile)
+					if '[RD53B_114]' in line:
+						print(f'mask_file = "../corry_mask_noisy_pixels/corry_output/MaskCreator/RD53B_114/mask_RD53B_114.txt.txt"', file=ofile)
 		
 		# Now create the config files from the templates.
 		arguments_for_config_files = {
 			'tell_corry_docker_to_run_this.sh': dict(
-				WORKING_DIRECTORY = str(utils.get_run_directory_within_corry_docker(bureaucrat)/employee.task_name),
+				WORKING_DIRECTORY = str(utils.get_run_directory_within_corry_docker(EUDAQ_run)/employee.task_name),
 				GEOMETRY_FILE_FOR_ALIGNMENT_ITERATIONS = "corry_output/align-telescope.geo",
 			),
 			'01_prealign-telescope.conf': dict(
@@ -140,41 +150,41 @@ def corry_align_telescope(bureaucrat:RunBureaucrat, corry_container_id:str, forc
 		# Make the script executable for docker:
 		subprocess.run(['chmod','+x',str(employee.path_to_directory_of_my_task/'tell_corry_docker_to_run_this.sh')])
 		
-		logging.info(f'Running corry telescope align on {bureaucrat.path_to_run_directory}...')
+		logging.info(f'Running corry telescope align on {EUDAQ_run.pseudopath}...')
 		result = utils.run_commands_in_docker_container(
-			command = str(utils.get_run_directory_within_corry_docker(bureaucrat)/employee.task_name/'tell_corry_docker_to_run_this.sh'),
+			command = str(utils.get_run_directory_within_corry_docker(EUDAQ_run)/employee.task_name/'tell_corry_docker_to_run_this.sh'),
 			container_id = corry_container_id,
 			stdout = subprocess.DEVNULL if silent_corry == True else None,
 			stderr = subprocess.STDOUT if silent_corry == True else None,
 		)
 		result.check_returncode()
-		logging.info(f'Telescope alignment was completed for {bureaucrat.pseudopath}✅')
+		logging.info(f'Telescope alignment was completed for {EUDAQ_run.pseudopath}✅')
 
-def corry_reconstruct_tracks_with_telescope(bureaucrat:RunBureaucrat, corry_container_id:str, force:bool=False, silent_corry:bool=False):
+def corry_reconstruct_tracks_with_telescope(EUDAQ_run:RunBureaucrat, corry_container_id:str, force:bool=False, silent_corry:bool=False):
 	"""Runs the routine to reconstruct the tracks using corryvreckan on
-	all the raw files of the run pointed to by `bureaucrat`. Additionally,
+	all the raw files of the run pointed to by `EUDAQ_run`. Additionally,
 	it creates an SQLite file with the tracks info which is easy to read,
 	as well as the ROOT file produced by corry."""
 	TEMPLATE_FILES_DIRECTORY = Path(__file__).parent.resolve()/Path('corry_templates/03_reconstruct_tracks')
 	
 	TASK_NAME = 'corry_reconstruct_tracks_with_telescope'
 	
-	if force==False and bureaucrat.was_task_run_successfully(TASK_NAME):
-		logging.info(f'Found an already successfull execution of {TASK_NAME} within {bureaucrat.run_name}, will not do anything.')
+	if force==False and EUDAQ_run.was_task_run_successfully(TASK_NAME):
+		logging.info(f'Found an already successfull execution of {TASK_NAME} within {EUDAQ_run.pseudopath}, will not do anything.')
 		return
 	
-	bureaucrat.check_these_tasks_were_run_successfully(['raw','corry_mask_noisy_pixels','corry_align_telescope'])
+	EUDAQ_run.check_these_tasks_were_run_successfully(['raw','corry_mask_noisy_pixels','corry_align_telescope'])
 	
-	with bureaucrat.handle_task(TASK_NAME) as employee:
+	with EUDAQ_run.handle_task(TASK_NAME) as employee:
 		arguments_for_config_files = {
 			'01_reconstruct_tracks.conf': dict(
 				OUTPUT_DIRECTORY = f'"corry_output"',
 				GEOMETRY_FILE = f'"../corry_align_telescope/corry_output/align-telescope-mille.geo"',
-				PATH_TO_RAW_FILE = f'"../raw/{bureaucrat.run_name}.raw"',
+				PATH_TO_RAW_FILE = f'"../raw/{EUDAQ_run.run_name}.raw"',
 				NAME_OF_OUTPUT_FILE_WITH_TRACKS = f'"tracks.root"',
 			),
 			'tell_corry_docker_to_run_this.sh': dict(
-				WORKING_DIRECTORY = str(utils.get_run_directory_within_corry_docker(bureaucrat)/employee.task_name),
+				WORKING_DIRECTORY = str(utils.get_run_directory_within_corry_docker(EUDAQ_run)/employee.task_name),
 			),
 			'corry_tracks_root2csv.py': dict(
 				PATH_TO_TRACKSdotROOT_FILE = f"'corry_output/tracks.root'",
@@ -192,16 +202,16 @@ def corry_reconstruct_tracks_with_telescope(bureaucrat:RunBureaucrat, corry_cont
 		for fname in arguments_for_config_files:
 			subprocess.run(['chmod','+x',str(employee.path_to_directory_of_my_task/fname)])
 		
-		logging.info(f'Reconstructing tracks with corry on {bureaucrat.path_to_run_directory}...')
+		logging.info(f'Reconstructing tracks with corry on {EUDAQ_run.pseudopath}...')
 		result = utils.run_commands_in_docker_container(
-			command = str(utils.get_run_directory_within_corry_docker(bureaucrat)/employee.task_name/'tell_corry_docker_to_run_this.sh'),
+			command = str(utils.get_run_directory_within_corry_docker(EUDAQ_run)/employee.task_name/'tell_corry_docker_to_run_this.sh'),
 			container_id = corry_container_id,
 			stdout = subprocess.DEVNULL if silent_corry == True else None,
 			stderr = subprocess.STDOUT if silent_corry == True else None,
 		)
 		result.check_returncode()
 		
-		logging.info(f'Converting the CSV file into an SQLite file on {bureaucrat.path_to_run_directory}...')
+		logging.info(f'Converting the CSV file into an SQLite file on {EUDAQ_run.pseudopath}...')
 		dtype = dict(
 			n_event = int,
 			n_track = int,
@@ -220,12 +230,12 @@ def corry_reconstruct_tracks_with_telescope(bureaucrat:RunBureaucrat, corry_cont
 				tracks_dumper.append(df)
 		logging.info('CSV to SQLite file conversion successful, CSV file will be removed.')
 		(employee.path_to_directory_of_my_task/'corry_output'/'tracks.csv').unlink()
-		logging.info(f'Tracks were reconstructed for {bureaucrat.pseudopath} ✅')
+		logging.info(f'Tracks were reconstructed for {EUDAQ_run.pseudopath} ✅')
 
-def corry_do_all_steps_in_some_run(run:RunBureaucrat, corry_container_id:str, force:bool=False, silent_corry:bool=False):
-	corry_mask_noisy_pixels(bureaucrat=run, corry_container_id=corry_container_id, force=force, silent_corry=silent_corry)
-	corry_align_telescope(bureaucrat=run, corry_container_id=corry_container_id, force=force, silent_corry=silent_corry)
-	corry_reconstruct_tracks_with_telescope(bureaucrat=run, corry_container_id=corry_container_id, force=force, silent_corry=silent_corry)
+def corry_do_all_steps_in_some_run(EUDAQ_run:RunBureaucrat, corry_container_id:str, force:bool=False, silent_corry:bool=False):
+	corry_mask_noisy_pixels(EUDAQ_run=EUDAQ_run, corry_container_id=corry_container_id, force=force, silent_corry=silent_corry)
+	corry_align_telescope(EUDAQ_run=EUDAQ_run, corry_container_id=corry_container_id, force=force, silent_corry=silent_corry)
+	corry_reconstruct_tracks_with_telescope(EUDAQ_run=EUDAQ_run, corry_container_id=corry_container_id, force=force, silent_corry=silent_corry)
 
 if __name__ == '__main__':
 	import sys
@@ -275,7 +285,7 @@ if __name__ == '__main__':
 	
 	utils.guess_where_how_to_run(
 		bureaucrat = bureaucrat,
-		raw_level_f = lambda bureaucrat: corry_do_all_steps_in_some_run(run=bureaucrat, corry_container_id = args.container_id, force = args.force,	silent_corry = not args.show_corry_output),
+		raw_level_f = lambda bureaucrat: corry_do_all_steps_in_some_run(EUDAQ_run=bureaucrat, corry_container_id = args.container_id, force = args.force,	silent_corry = not args.show_corry_output),
 		telegram_bot_reporter = SafeTelegramReporter4Loops(
 			bot_token = my_telegram_bots.robobot.token,
 			chat_id = my_telegram_bots.chat_ids['Robobot TCT setup'],
