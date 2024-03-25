eeaa69ceba1c664757177cd93e221d064cf08fa6
Matias Senger <m.senger@hotmail.com>
2024-03-25 13:29:33 +0100

    Reimplemented `load_setup_configuration_info`

--- utils_batch_level.py
+++ utils_batch_level.py
@@ -117,92 +117,92 @@ def setup_batch_info(TB_batch_dn:DatanodeHandler):
 			raise RuntimeError(f'Cannot determine which test beam campaign {TB_batch_dn.pseudopath} belongs to...')
 	logging.info(f'Setup info was set for batch "{TB_batch_dn.pseudopath}" ✅')
 
-# ~ def load_setup_configuration_info(TB_batch:RunBureaucrat)->pandas.DataFrame:
-	# ~ TB_batch.check_these_tasks_were_run_successfully(['EUDAQ_runs','batch_info'])
-	# ~ if not all([b.was_task_run_successfully('parse_waveforms') for b in TB_batch.list_subruns_of_task('EUDAQ_runs')]):
-		# ~ raise RuntimeError(f'To load the setup configuration it is needed that all of the runs of the batch have had the `parse_waveforms` task performed on them, but does not seem to be the case')
+def load_setup_configuration_info(TB_batch_dn:DatanodeHandler)->pandas.DataFrame:
+	TB_batch_dn.check_datanode_class('TB_batch')
 	
-	# ~ if TB_batch.was_task_run_successfully('voltage_batch'):
-		# ~ TB_batch_voltage = TB_batch
-		# ~ TB_batch = TB_batch.parent
+	# Check that all the tasks we need were properly executed already:
+	TB_batch_dn.check_these_tasks_were_run_successfully(['EUDAQ_runs','batch_info'])
+	if not all([dn.was_task_run_successfully('parse_waveforms') for dn in TB_batch_dn.list_subdatanodes_of_task('EUDAQ_runs')]):
+		raise RuntimeError(f'To load the setup configuration it is needed that all of the runs of the batch have had the `parse_waveforms` task performed on them, but does not seem to be the case')
 	
-	# ~ match TB_batch.parent.run_name: # This is the test beam campaign, the parent of every batch.
-		# ~ case '230614_June':
-			# ~ planes = pandas.read_pickle(TB_batch.path_to_directory_of_task('batch_info')/'planes.pickle')
-			# ~ signals_connections = pandas.read_pickle(TB_batch.path_to_directory_of_task('batch_info')/'signals.pickle')
-		# ~ case '230830_August':
-			# ~ planes = pandas.read_pickle(TB_batch.path_to_directory_of_task('batch_info')/'planes_definition.pickle')
-			# ~ signals_connections = pandas.read_pickle(TB_batch.path_to_directory_of_task('batch_info')/'pixels_definition.pickle')
-			# ~ for df in [planes,signals_connections]:
-				# ~ df.rename(
-					# ~ columns = {
-						# ~ 'digitizer_name': 'CAEN_name',
-						# ~ 'digitizer_channel_name': 'CAEN_channel_name',
-						# ~ 'chubut_channel_number': 'chubut_channel',
-					# ~ },
-					# ~ inplace = True,
-				# ~ )
-		# ~ case '240212_DESY':
-			# ~ planes = pandas.read_pickle(TB_batch.path_to_directory_of_task('batch_info')/'planes_definition.pickle')
-			# ~ signals_connections = pandas.read_pickle(TB_batch.path_to_directory_of_task('batch_info')/'pixels_definition.pickle')
-			# ~ for df in [planes,signals_connections]:
-				# ~ df.rename(
-					# ~ columns = {
-						# ~ 'digitizer_name': 'CAEN_name',
-						# ~ 'digitizer_channel_name': 'CAEN_channel_name',
-						# ~ 'chubut_channel_number': 'chubut_channel',
-					# ~ },
-					# ~ inplace = True,
-				# ~ )
-		# ~ case _:
-			# ~ raise RuntimeError(f'Cannot read setup information for run {TB_batch.run_name}')
+	TB_campaign = TB_batch_dn.parent # The parent of the batch should be the TB campaign.
+	if TB_campaign is None or TB_campaign.datanode_class != 'TB_campaign':
+		raise RuntimeError(f'I was expecting that the parent of batch "{TB_batch_dn.pseudopath}" is a test beam campaign, but it is not. ')
 	
-	# ~ if TB_batch.was_task_run_successfully('voltage_batch'):
-		# ~ pass
+	match TB_campaign.datanode_name: # This is the test beam campaign, the parent of every batch.
+		case '230614_June':
+			planes = pandas.read_pickle(TB_batch_dn.path_to_directory_of_task('batch_info')/'planes.pickle')
+			signals_connections = pandas.read_pickle(TB_batch_dn.path_to_directory_of_task('batch_info')/'signals.pickle')
+		case '230830_August':
+			planes = pandas.read_pickle(TB_batch_dn.path_to_directory_of_task('batch_info')/'planes_definition.pickle')
+			signals_connections = pandas.read_pickle(TB_batch_dn.path_to_directory_of_task('batch_info')/'pixels_definition.pickle')
+			for df in [planes,signals_connections]:
+				df.rename(
+					columns = {
+						'digitizer_name': 'CAEN_name',
+						'digitizer_channel_name': 'CAEN_channel_name',
+						'chubut_channel_number': 'chubut_channel',
+					},
+					inplace = True,
+				)
+		case '240212_DESY':
+			planes = pandas.read_pickle(TB_batch_dn.path_to_directory_of_task('batch_info')/'planes_definition.pickle')
+			signals_connections = pandas.read_pickle(TB_batch_dn.path_to_directory_of_task('batch_info')/'pixels_definition.pickle')
+			for df in [planes,signals_connections]:
+				df.rename(
+					columns = {
+						'digitizer_name': 'CAEN_name',
+						'digitizer_channel_name': 'CAEN_channel_name',
+						'chubut_channel_number': 'chubut_channel',
+					},
+					inplace = True,
+				)
+		case _:
+			raise RuntimeError(f'Cannot read setup information for batch "{TB_batch_dn.datanode_name}". ')
 	
-	# ~ CAENs_names = []
-	# ~ for run in TB_batch.list_subruns_of_task('EUDAQ_runs'):
-		# ~ n_run = int(run.run_name.split('_')[0].replace('run',''))
-		# ~ df = pandas.read_pickle(run.path_to_directory_of_task('parse_waveforms')/f'{run.run_name}_CAENs_names.pickle')
-		# ~ df = df.to_frame()
-		# ~ df['n_run'] = n_run
-		# ~ df.set_index('n_run',append=True,inplace=True)
-		# ~ CAENs_names.append(df)
-	# ~ CAENs_names = pandas.concat(CAENs_names)
-	# ~ CAENs_names['CAEN_name'] = CAENs_names['CAEN_name'].apply(lambda x: x.replace('CAEN_',''))
+	CAENs_names = []
+	for run_dn in TB_batch_dn.list_subdatanodes_of_task('EUDAQ_runs'):
+		n_run = int(run_dn.datanode_name.split('_')[0].replace('run',''))
+		df = pandas.read_pickle(run_dn.path_to_directory_of_task('parse_waveforms')/f'parsed_from_waveforms_CAENs_names.pickle')
+		df = df.to_frame()
+		df['n_run'] = n_run
+		df.set_index('n_run',append=True,inplace=True)
+		CAENs_names.append(df)
+	CAENs_names = pandas.concat(CAENs_names)
+	CAENs_names['CAEN_name'] = CAENs_names['CAEN_name'].apply(lambda x: x.replace('CAEN_',''))
 	
-	# ~ # Here we assume that the CAENs were not changed within a batch, which is reasonable.
-	# ~ _ = CAENs_names.reset_index('n_CAEN',drop=False).set_index('CAEN_name',append=True).reset_index('n_run',drop=True)
-	# ~ _ = _[~_.index.duplicated(keep='first')]
-	# ~ signals_connections = signals_connections.reset_index(drop=False).merge(
-		# ~ _,
-		# ~ on = 'CAEN_name',
-	# ~ )
+	# Here we assume that the CAENs were not changed within a batch, which is not only reasonable but also what we expect.
+	_ = CAENs_names.reset_index('n_CAEN',drop=False).set_index('CAEN_name',append=True).reset_index('n_run',drop=True)
+	_ = _[~_.index.duplicated(keep='first')]
+	signals_connections = signals_connections.reset_index(drop=False).merge(
+		_,
+		on = 'CAEN_name',
+	)
 	
-	# ~ CAENs_CHANNELS_MAPPING_TO_INTEGERS = pandas.DataFrame(
-		# ~ # This codification into integers comes from the producer, see in line 208 of `CAENDT5742Producer.py`. The reason is that EUDAQ can only handle integers tags, or something like this.
-		# ~ {
-			# ~ 'CAEN_n_channel': list(range(18)),
-			# ~ 'CAEN_channel_name': [f'CH{i}' if i<16 else f'trigger_group_{i-16}' for i in range(18)]
-		# ~ }
-	# ~ )
+	CAENs_CHANNELS_MAPPING_TO_INTEGERS = pandas.DataFrame(
+		# This codification into integers comes from the producer, see in line 208 of `CAENDT5742Producer.py`. The reason is that EUDAQ can only handle integers tags, or something like this.
+		{
+			'CAEN_n_channel': list(range(18)),
+			'CAEN_channel_name': [f'CH{i}' if i<16 else f'trigger_group_{i-16}' for i in range(18)]
+		}
+	)
 	
-	# ~ signals_connections = signals_connections.merge(
-		# ~ CAENs_CHANNELS_MAPPING_TO_INTEGERS.set_index('CAEN_channel_name')['CAEN_n_channel'],
-		# ~ on = 'CAEN_channel_name',
-	# ~ )
+	signals_connections = signals_connections.merge(
+		CAENs_CHANNELS_MAPPING_TO_INTEGERS.set_index('CAEN_channel_name')['CAEN_n_channel'],
+		on = 'CAEN_channel_name',
+	)
 	
-	# ~ signals_connections = signals_connections.merge(
-		# ~ planes[['DUT_name','z (m)']],
-		# ~ on = 'plane_number',
-	# ~ )
+	signals_connections = signals_connections.merge(
+		planes[['DUT_name','z (m)']],
+		on = 'plane_number',
+	)
 	
-	# ~ signals_connections['CAEN_trigger_group_n'] = signals_connections['CAEN_n_channel'].apply(lambda x: 0 if x in {0,1,2,3,4,5,6,7,16} else 1 if x in {8,9,10,11,12,13,14,15,17} else -1)
+	signals_connections['CAEN_trigger_group_n'] = signals_connections['CAEN_n_channel'].apply(lambda x: 0 if x in {0,1,2,3,4,5,6,7,16} else 1 if x in {8,9,10,11,12,13,14,15,17} else -1)
 	
-	# ~ signals_connections['rowcol'] = signals_connections[['row','col']].apply(lambda x: f'{x["row"]}{x["col"]}', axis=1)
-	# ~ signals_connections['DUT_name_rowcol'] = signals_connections[['DUT_name','row','col']].apply(lambda x: f'{x["DUT_name"]} ({x["row"]},{x["col"]})', axis=1)
+	signals_connections['rowcol'] = signals_connections[['row','col']].apply(lambda x: f'{x["row"]}{x["col"]}', axis=1)
+	signals_connections['DUT_name_rowcol'] = signals_connections[['DUT_name','row','col']].apply(lambda x: f'{x["DUT_name"]} ({x["row"]},{x["col"]})', axis=1)
 	
-	# ~ return signals_connections
+	return signals_connections
 
 # ~ def load_parsed_from_waveforms(TB_batch:RunBureaucrat, load_this:dict, variables:list=None)->pandas.DataFrame:
 	# ~ """Load data parsed from the waveforms for all the runs within a batch.
