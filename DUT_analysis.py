642e4649c1b848ded8207f2feeb586aade9907c7
Matias Senger <m.senger@hotmail.com>
2024-03-26 22:35:28 +0100

    Things

--- DUT_analysis.py
+++ DUT_analysis.py
@@ -4,6 +4,9 @@ import pandas
 import logging
 import utils_batch_level
 import json
+import utils_voltage_level
+import dominate
+import dominate.tags as tags
 
 def create_DUT_analysis(TB_batch_dn:DatanodeHandler, DUT_name:str, plane_number:int, chubut_channel_numbers:set):
 	"""Creates a datanode to handle the analysis of one DUT.
@@ -73,6 +76,49 @@ def create_voltage_point(DUT_analysis_dn:DatanodeHandler, voltage:int, EUDAQ_run
 				)
 	logging.info(f'Voltage point {repr(str(voltage_dn.pseudopath))} was created. ✅')
 
+def load_DUT_configuration_metadata(DUT_analysis_dn:DatanodeHandler):
+	DUT_analysis_dn.check_datanode_class('DUT_analysis')
+	with open(DUT_analysis_dn.path_to_directory_of_task('setup_config_metadata')/'metadata.json', 'r') as ifile:
+		loaded = json.load(ifile)
+		this_DUT_chubut_channels = loaded['chubut_channels_numbers']
+		this_DUT_plane_number = loaded['plane_number']
+	if len(this_DUT_chubut_channels) == 0:
+		raise RuntimeError(f'No `chubut channels` associated with DUT in {repr(str(DUT_analysis_dn.pseudopath))}. ')
+	if not isinstance(this_DUT_plane_number, int):
+		raise RuntimeError(f'Cannot determine plane number for DUT in {repr(str(DUT_analysis_dn.pseudopath))}. ')
+	
+	return loaded
+
+def plot_waveforms_distributions(DUT_analysis_dn:DatanodeHandler, max_points_to_plot_per_voltage=9999, histograms=['Amplitude (V)','Collected charge (V s)','t_50 (s)','Rise time (s)','SNR','Time over 50% (s)'], scatter_plots=[('t_50 (s)','Amplitude (V)'),('Time over 50% (s)','Amplitude (V)')]):
+	
+	with DUT_analysis_dn.handle_task('plot_waveforms_distributions', 'DUT_analysis', 'voltages') as task:
+		for voltage_dn in DUT_analysis_dn.list_subdatanodes_of_task('voltages'):
+			utils_voltage_level.plot_waveforms_distributions(
+				voltage_point_dn = voltage_dn,
+				max_points_to_plot = max_points_to_plot_per_voltage,
+				histograms = histograms,
+				scatter_plots = scatter_plots,
+			)
+		
+		voltages = sorted(DUT_analysis_dn.list_subdatanodes_of_task('voltages'), key=DatanodeHandler.datanode_name.__get__)
+		
+		for kind_of_plot in {'histograms','scatter_plots'}:
+			save_plots_here = task.path_to_directory_of_my_task/kind_of_plot
+			save_plots_here.mkdir()
+			
+			for plot_file_name in [_.name for _ in (DUT_analysis_dn.list_subdatanodes_of_task('voltages')[0].path_to_directory_of_task('plot_waveforms_distributions')/kind_of_plot).iterdir()]:
+				doc = dominate.document(title=plot_file_name.split('.')[0])
+				with doc:
+					tags.h1(plot_file_name.split('.')[0] + ' distributions')
+					tags.h3(str(DUT_analysis_dn.pseudopath))
+					for voltage_dn in voltages:
+						tags.iframe(
+							src = Path('../..')/((voltage_dn.path_to_directory_of_task('plot_waveforms_distributions')/kind_of_plot/plot_file_name).relative_to(DUT_analysis_dn.path_to_datanode_directory)),
+							style = 'height: 90vh; width: 100%; min-height: 666px; min-width: 666px; border: 0;',
+						)
+				with open(save_plots_here/plot_file_name, 'w') as ofile:
+					print(doc, file=ofile)
+
 if __name__ == '__main__':
 	import sys
 	import argparse
@@ -93,21 +139,8 @@ if __name__ == '__main__':
 		dest = 'datanode',
 		type = Path,
 	)
-	parser.add_argument('--voltage',
-		help = 'Value of voltage.',
-		dest = 'voltage',
-		type = int,
-	)
-	parser.add_argument('--EUDAQ_runs',
-		help = 'EUDAQ run numbers as integers',
-		dest = 'EUDAQ_runs',
-		type = int,
-		nargs = '+',
-	)
 	args = parser.parse_args()
 	
-	create_voltage_point(
+	plot_waveforms_distributions(
 		DUT_analysis_dn = DatanodeHandler(args.datanode), 
-		voltage = args.voltage,
-		EUDAQ_runs = args.EUDAQ_runs,
 	)
