7aed4f079f892ee914444819142ec2d0417d941c
Matias Senger <m.senger@hotmail.com>
2024-04-04 12:25:36 +0200

    Fixed bug introduced recently

--- parse_waveforms.py
+++ parse_waveforms.py
@@ -205,11 +205,11 @@ def load_parsed_from_waveforms_from_EUDAQ_run(EUDAQ_run_dn:DatanodeHandler, wher
 	
 	logging.info(f'Reading {variables} from {EUDAQ_run_dn.pseudopath}...')
 	
-	if len(variables) == 0:
-		variables = None
 	if isinstance(variables, str):
 		variables = [variables]
 	if variables is not None:
+		if len(variables) == 0:
+			variables = None
 		variables = ',' + ','.join([f'`{_}`' for _ in variables])
 	else:
 		variables = ''
